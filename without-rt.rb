467a5bc24e8c615fc416a3ebecddbc18c2299543
Bryce Kerley <bkerley@brycekerley.net>
2009-02-22 08:01:23 -0500

    simple filters actually work

--- without-rt.rb
+++ without-rt.rb
@@ -1,6 +1,20 @@
 require 'rubygems'
 require 'sinatra'
+require 'twitter'
+
+t = Twitter::Base.new('without_rt', 'stop_all_retweets')
 
 get '/' do
   "set your twitter client to point at this address instead of real twitter"
+end
+
+get '/statuses/friends_timeline.:format' do
+  all_statuses = t.timeline
+  filtered_statuses = all_statuses.reject{ |s| looks_like_rt s }
+  filtered_statuses.map{|s| "#{s.user.name}: #{s.text}"}.join("<br \>")
+end
+
+def looks_like_rt(status)
+  return true if status.text =~ /^RT\s/
+  return true if status.text[0..40] =~ /re-?tweet/
 end
